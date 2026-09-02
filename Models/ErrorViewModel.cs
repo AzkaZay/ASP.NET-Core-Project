cf80da55b5177516c184d6d02bdd5727fce7dfd2
zainab.azka.beesony <zainab.azka.beesony@MAEGL0M6G>
2026-09-02 22:33:15 +0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PatisserieCD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
