c57483e11fc83e610aaf761a340fe107a71ba081
AzkaZay <azkazay000@gmail.com>
2026-09-22 03:23:36 +0400

    Updating Models and adding a razor file

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,10 @@
-﻿namespace PatisserieCD.Models
-{
+﻿namespace PatisserieCD.Models;
+
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int PreparationLeadTimeHours { get; set; }
     }
-}
