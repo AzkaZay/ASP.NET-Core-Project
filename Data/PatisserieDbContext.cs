14e84d4a713379df4e79b5b5c911f5e124324786
AzkaZay <azkazay000@gmail.com>
2026-09-24 23:03:50 +0400

    Order dbContext

--- Data/PatisserieDbContext.cs
+++ Data/PatisserieDbContext.cs
@@ -12,4 +12,7 @@ public class PatisserieDbContext : DbContext
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 }
