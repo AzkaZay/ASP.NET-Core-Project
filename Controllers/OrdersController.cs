956aaeaa4c56bd960c4c72f4ded6fa14ee6dfa7a
AzkaZay <azkazay000@gmail.com>
2026-09-24 23:17:00 +0400

    order's controller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatisserieCD.Models;
+using PatisserieCD.Services;
+
+namespace PatisserieCD.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    [HttpGet]
+    public IActionResult GetOrders()
+    {
+        var orders = _orderService.GetOrders();
+
+        return Ok(orders);
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetOrder(int id)
+    {
+        var order = _orderService.GetOrderById(id);
+
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(order);
+    }
+    [HttpPost]
+    public IActionResult CreateOrder(Order order)
+    {
+        _orderService.AddOrder(order);
+
+        return Ok(order);
+    }
+}
