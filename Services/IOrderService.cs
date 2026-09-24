8e909aaa19e58ac9b91cfcc12aab02fdd332f1b9
AzkaZay <azkazay000@gmail.com>
2026-09-24 23:04:31 +0400

    Order services

---
+++ Services/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using PatisserieCD.Models;
+
+namespace PatisserieCD.Services;
+
+public interface IOrderService
+{
+    List<Order> GetOrders();
+    Order? GetOrderById(int id);
+    void AddOrder(Order order);
+}
---
+++ Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatisserieCD.Data;
+using PatisserieCD.Models;
+
+namespace PatisserieCD.Services;
+public class OrderService : IOrderService
+{
+    private readonly PatisserieDbContext _context;
+
+    public OrderService(PatisserieDbContext context)
+    {
+        _context = context;
+    }
+    public List<Order> GetOrders()
+    {
+        return _context.Orders
+            .Include(o => o.Customer)
+            .Include(o => o.OrderItems)
+            .ToList();
+    }
+    public Order? GetOrderById(int id)
+    {
+        return _context.Orders
+            .Include(o => o.Customer)
+            .Include(o => o.OrderItems)
+            .FirstOrDefault(o => o.Id == id);
+    }
+    public void AddOrder(Order order)
+    {
+        _context.Orders.Add(order);
+        _context.SaveChanges();
+    }
+}
