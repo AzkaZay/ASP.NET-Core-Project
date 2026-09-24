583ef9d3f72a7533cd0ecb488467ec86cdba58a5
AzkaZay <azkazay000@gmail.com>
2026-09-24 22:10:02 +0400

    Products Controllerfirst HttpPost

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,42 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PatisserieCD.Models;
 using PatisserieCD.Services;
 
-namespace PatisserieCD.Controllers
-{
-    public class ProductsController : Controller
-    {
-        private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
-        {
-            _productService = productService;
-        }
-        public IActionResult Index()
-        {
-            var products = _productService.GetProducts();
+namespace PatisserieCD.Controllers;
 
-            return View(products);
-        }
-        public IActionResult Details(int id)
+public class ProductsController : Controller
+{
+    private readonly IProductService _productService;
+    private readonly ICategoryService _categoryService;
+
+    public ProductsController(
+        IProductService productService,
+        ICategoryService categoryService)
+    {
+        _productService = productService;
+        _categoryService = categoryService;
+    }
+
+    public IActionResult Index()
+    {
+        var products = _productService.GetProducts();
+
+        return View(products);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var product = _productService.GetProductById(id);
+
+        return View(product);
+    }
+
+    public IActionResult Create()
+    {
+        var categories = _categoryService.GetCategories();
+
+        ViewBag.Categories = categories;
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        if (!ModelState.IsValid)
         {
-            var product = _productService.GetProductById(id);
+            ViewBag.Categories = _categoryService.GetCategories();
 
             return View(product);
         }
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Product product)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(product);
-            }
 
-            _productService.AddProduct(product);
+        _productService.AddProduct(product);
 
-            return RedirectToAction("Index");
-        }
+        return RedirectToAction("Index");
     }
 }
